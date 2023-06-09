12e7ac8dbae9cd4c302812758ef5a009c4804ff4
bielak84 <69897588+bielak84@users.noreply.github.com>
2023-06-09 15:39:16 -0700

    Update connect.php
    komunikat i przerwanie jeżeli error przy połączeniu

--- scripts/connect.php
+++ scripts/connect.php
@@ -2,4 +2,8 @@
 
     $dbconn = pg_connect("dbname=localhost port=5432 dbname=studentolx user=postgres password=P05tGr3s");
 
+if (!$dbconn) {
+        die("Błąd połączenia z bazą danych: " . pg_last_error());
+    }
+
 ?>
