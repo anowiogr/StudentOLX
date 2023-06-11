8771df84380554522c64f0e7bcfde3782a03890c
Iwona <iwona.ogrodowska@epoczta.pl>
2023-06-11 12:48:39 +0200

    #008 IO Modyfikacja nagłówka i stopki na osobne pliki

---
+++ constant/footer.php
@@ -0,0 +1,7 @@
+<footer class="mt-auto bg-dark">
+    <div class="container">
+        <div class="p-2 mb-1 col-lg-12 text-secondary">
+            © Created by: Iwona Ogrodowska, Karolina Małecka, Łukasz Bielski
+        </div>
+    </div>
+</footer>
