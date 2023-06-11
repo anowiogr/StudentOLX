b8b4628596afe54c7dc90cf35d0ab70fa6070f14
bielak <bielski.lukas@gmail.com>
2023-06-11 10:55:30 -0700

    dodałem guzik cofnij, history back

--- constant/footer.php
+++ constant/footer.php
@@ -3,6 +3,13 @@
         <div class="p-2 mb-1 col-lg-12 text-secondary">
             © Created by: Iwona Ogrodowska, Karolina Małecka, Łukasz Bielski
         </div>
+        <button onclick="goBack()">Cofnij</button>
+
+<script>
+    function goBack() {
+        window.history.back();
+    }
+</script>
     </div>
 </footer>
 </html>
