d014992dec737476fd1dc129f38886a4fe4704bc
Iwona <iwona.ogrodowska@epoczta.pl>
2023-06-12 22:48:02 +0200

    020A IO Dokończenie menu

--- constant/header.php
+++ constant/header.php
@@ -30,88 +30,99 @@
 
 
 
-<nav class="navbar navbar-expand-sm fixed-top navbar-dark bg-dark" style="position: relative;">
-
-    <div class="container">
-
-        <a href="./index.php"><img src="./images/icon.png" class="bi" width="40" height="40" /></a>
-
-        <button type="button" data-bs-toggle="collapse" data-bs-target="#navbar-menu" class="navbar-toggler justify-content-center" aria-controls="navbar-menu" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
+       <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+           <div class="container">
+            <a class="navbar-brand" href="./index.php"><img src="./images/icon.png" class="bi" width="40" height="40" /></a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbar-menu" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
 
         <?php
             if($_SESSION["role"]=="guest"){
                 ?>
-            <div class="collapse navbar-collapse text-secondary justify-content-end" id="navbar-menu">
-
-            <ul class="navbar-nav">
-                <br />
-                <li class="nav-item active">
-                    <a href="./login.php"><button type="button" class="btn btn-outline-light me-2">Zaloguj</button></a>
 
 
-                </li>
-                <br />
-                <li class="nav-item active">
-                    <a href="./register.php"><button type="button" class="btn btn-light">Zarejestruj się</button></a>
-                </li>
-            </ul>
 
-        </div>
+               <div class="collapse navbar-collapse text-secondary justify-content-end" id="navbar-menu">
+                    <ul class="navbar-nav mr-auto">
+                        <li class="nav-item">
+                            <a href="./login.php"><button type="button" class="btn btn-outline-light me-2">Zaloguj</button></a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="./register.php"><button type="button" class="btn btn-light">Zarejestruj się</button></a>
+                        </li>
+                    </ul>
+
+
+
+
+
 
         <?php
             }elseif ($_SESSION["role"]=="user"){
         ?>
 
-        <div class="collapse navbar-collapse text-secondary justify-content-end" id="navbar-menu">
-            <ul class="navbar-nav mr-auto">
-                <li class="nav-item">
-                    <a class="nav-link" href="./userauctions.php">Aukcje</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="./wiadomosci.php">Wiadomości</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="./userabout.php">Ustawienia</a>
-                </li>
-                <li class="nav-item active">
-                        <a href="./scripts/logout.php"><button type="button" class="btn btn-danger">Wyloguj</button></a>
-                </li>
-            </ul>
-        </div>
+                   <div class="collapse navbar-collapse text-secondary justify-content-end" id="navbar-menu">
+                        <ul class="navbar-nav mr-auto">
+                            <li class="nav-item">
+                                <a class="nav-link" href="./userauctions.php">Aukcje</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="./wiadomosci.php">Wiadomości</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="./userabout.php">Ustawienia</a>
+                            </li>
+                            <li class="nav-item active">
+                                <a href="./scripts/logout.php"><button type="button" class="btn btn-danger">Wyloguj</button></a>
+                            </li>
+                        </ul>
+
+
                 <?php
         }elseif ($_SESSION["role"]=="admin"){
         ?>
 
-                <div class="collapse navbar-collapse text-secondary justify-content-end" id="navbar-menu">
-                    <ul class="navbar-nav mr-auto">
-                        <li class="nav-item">
-                            <a class="nav-link" href="./userauctions.php">Aukcje</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="./wiadomosci.php">Wiadomości</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Administracja</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="./userabout.php">Ustawienia</a>
-                        </li>
-                        <li class="nav-item active">
-                            <a href="./scripts/logout.php"><button type="button" class="btn btn-danger">Wyloguj</button></a>
-                        </li>
-                    </ul>
-                </div>
+                       <div class="collapse navbar-collapse text-secondary justify-content-end" id="navbar-menu">
+                            <ul class="navbar-nav mr-auto">
+                                <li class="nav-item">
+                                    <a class="nav-link" href="./userauctions.php">Aukcje</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="./wiadomosci.php">Wiadomości</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#">Administracja</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="./userabout.php">Ustawienia</a>
+                                </li>
+                                <li class="nav-item active">
+                                    <a href="./scripts/logout.php"><button type="button" class="btn btn-danger">Wyloguj</button></a>
+                                </li>
+                            </ul>
 
                 <?php
         }
         ?>
-
     </div>
+  </div>
 </nav>
 <body>
-<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <div class="container">
+        <div class="row col-md-12 p-3">
+            <div class="input-group rounded">
+                <input type="search" class="form-control rounded" placeholder="Co dzisiaj szukasz?" aria-label="Search" aria-describedby="search-addon">
+                <span class="input-group-text border-0" id="search-addon">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
+                    <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
+                    </svg>
+                </span>
+            </div>
+        </div>
+
+    </div>
+
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
 
 </body>
