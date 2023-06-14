98019c2c77f6ef35a6eed6fac5b67fbd62ceb904
Iwona <iwona.ogrodowska@epoczta.pl>
2023-06-14 21:58:41 +0200

    043 IO Rozszerzenie formularza aukcji o nowe pola

--- addauction.php
+++ addauction.php
@@ -2,14 +2,9 @@
 require 'constant/header.php';
 require 'scripts/connect.php';
 
-$title = '';
-$description = '';
 $used = false;
 $private = false;
-$dateStart = date('Y-m-d');
-$dateEnd = date('Y-m-d', strtotime('+2 weeks'));
-$account_id = $_SESSION["logged"]["account_id"];
-//print_r($_SESSION["logged"]);
+
 
     try {
         $pdo = new PDO("mysql:host=$host;dbname=$dbname", $user, $password);
@@ -19,31 +14,76 @@ $account_id = $_SESSION["logged"]["account_id"];
         echo "Błąd połączenia: " . $e->getMessage();
     }
 
+
+$account_id = $_SESSION["logged"]["account_id"];
+
+
     // Wyświetlanie formularza dodawania nowego ogłoszenia
-    echo <<<TABLEFORM
-         <form method='POST' action='scripts/newauction.php'>
-          <div class="form-row p-3">
-         <label for='title'>Tytuł:</label><br>
-         <input class="form-control" type='text' name='title' id='title' value='$title' required><br>
-    
-         <label for='description'>Opis:</label><br>
-         <textarea class="form-control" name='description' id='description' required>$description</textarea><br>
-         
-         <label class="form-check-label" for='used'>Używany: </label>
-         <input class="form-check-input" type='checkbox' name='used' id='used' <?php echo "($used ? 'checked' : '')"; ?> <br>
-    
-         <label class="form-check-label"for='private'>Prywatny:</label>
-         <input class="form-check-input" type='checkbox' name='private' id='private' <?php echo "($private ? 'checked' : '')"; ?> <br>
-    
-         <input type='hidden' name='account_id' value='$account_id'> <!-- ID użytkownika, dla którego dodawane jest ogłoszenie-->
-            <br>
-         <button class="btn btn-secondary" type='submit' name='action' value='add'>Dodaj</button>
-         </div>
-        </form>
-    TABLEFORM;
-
-
 ?>
+         <form method='POST' action='scripts/newauction.php'>
+         <table class="table">
+            <tbody>
+            <tr>
+                <td><label for='title'>Tytuł:</label></td>
+                <td><input class="form-control" type='text' name='title' id='title' required></td>
+            </tr>
+            <tr>
+                <td><label for='categoryid'>Kategoria:</label></td>
+                <td>
+                    <select class="form-control" name='categoryid' id='categoryid' required>
+                    <?php
+                      $query = "SELECT * FROM category";
+                      $stmt = $pdo->query($query);
+                      $category1 = $stmt->fetchAll();
+                      //print_r($category);
+                      foreach ($category1 as $category){
+                        echo "<option value='$category[categoryid]'>$category[name]</option>";
+                      }
+                      ?>
+                    </select>
+                </td>
+            </tr>
+            <tr>
+                <td><label for='description'>Opis:</label></td>
+                <td><textarea class="form-control" name='description' id='description' required></textarea></td>
+            </tr>
+            <tr>
+                <td><label class="form-check-label" for='used'>Używany: </label></td>
+                <td><input class="form-check-input" type='checkbox' name='used' id='used' > </td>
+            </tr>
+            <tr>
+                <td><label class="form-check-label"for='private'>Prywatny:</label></td>
+                <td><input class="form-check-input" type='checkbox' name='private' id='private' ></td>
+            </tr>
+            <tr>
+                <td colspan="2">Cena:</td>
+            </tr>
+
+            <tr>
+                <td><input class="form-control" type='text' pattern="\d*" name='price' id='price' required></td>
+                <td><select class="form-control" name='currencyid' id='currencyid' required>
+                        <?php
+                        $query = "SELECT * FROM currency";
+                        $stmt = $pdo->query($query);
+                        $currency1 = $stmt->fetchAll();
+                        foreach ($currency1 as $currency){
+                            echo "<option value='$currency[currencyid]'>$currency[currency_name]</option>";
+                        }
+                        ?>
+                    </select>
+                </td>
+            </tr>
+            <tr>
+                <td colspan="2">
+                 <input type='hidden' name='account_id' <?php echo "value=' $account_id '";?>> <!-- ID użytkownika, dla którego dodawane jest ogłoszenie-->
+                 <button class="btn btn-secondary" type='submit' name='action' value='add'>Dodaj</button>
+                </td>
+            </tr>
+
+         </tbody>
+         </table>
+        </form>
+
 
 
 <?php
--- scripts/newauction.php
+++ scripts/newauction.php
@@ -7,19 +7,20 @@ try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname", $user, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-$query = "INSERT INTO auctions (title, description, used, private, date_start, date_end, accountid) VALUES (:title, :description, :used, :private, current_timestamp, (current_timestamp+30), :accountId)";
+$query = "INSERT INTO auctions (title, description, used, private, categoryid, currencyid, price,  date_end, accountid) 
+            VALUES (:title, :description, :used, :private, :categoryid, :currencyid, :price, (current_timestamp+30), :accountId)";
 $stmt = $pdo->prepare($query);
 $stmt->bindParam(':title', $_POST["title"]);
 $stmt->bindParam(':description', $_POST["description"]);
 $stmt->bindParam(':used', $_POST["used"], PDO::PARAM_BOOL);
 $stmt->bindParam(':private', $_POST["private"], PDO::PARAM_BOOL);
-//$stmt->bindParam(':dateStart', $dateStart);
-//$stmt->bindParam(':dateEnd', $dateEnd);
+$stmt->bindParam(':categoryid', $_POST["categoryid"], PDO::PARAM_INT);
+$stmt->bindParam(':currencyid', $_POST["currencyid"], PDO::PARAM_INT);
+$stmt->bindParam(':price', $_POST["price"]);
 $stmt->bindParam(':accountId', $_POST["account_id"], PDO::PARAM_INT);
-
 $stmt->execute();
 
-    header("location: ../userauctions.php");
+header("location: ../userauctions.php");
 
 } catch (PDOException $e) {
     echo "Błąd połączenia: " . $e->getMessage();
