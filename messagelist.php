27ce8826f29a901747905ef7b568c3c1a2182c19
Iwona <iwona.ogrodowska@epoczta.pl>
2023-06-15 11:43:51 +0200

    050 IO Dodanow strony obslugujace wiadomosci wewnetrzne

--- messagelist.php
+++ messagelist.php
@@ -2,13 +2,10 @@
 require "constant/header.php";
 require 'scripts/connect.php';
 
-// Sprawdzenie czy użytkownik jest zalogowany
 if (!isset($_SESSION['logged']['account_id'])||$_SESSION['logged']['account_id']==null) {
     header("Location: index.php");
 
 }
-
-
 $account_id = $_SESSION['logged']['account_id'];
 
 try {
@@ -16,31 +13,27 @@ try {
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $pdo->exec("USE $dbname");
 
-    //Zapytanie dla nagłówków wiadomości sprzedawanych
-    $querySell = "SELECT auctions.title, auctions.auctionid
-                  FROM message
-                  INNER JOIN auctions ON message.auctionid = auctions.auctionid
-                  WHERE message.mlid IN (SELECT mlid FROM message_link WHERE sellerid = :account_id)
-                  AND auctions.accountid = :account_id";
+    //Zapytanie dla sprzedawanych przedmiotów (naglowek)
+    $querySell = "SELECT 
+    DISTINCT m.auctionid, m.buyerid, x.accountid as seller, a.login, x.title
+    FROM message m 
+     LEFT JOIN auctions x ON m.auctionid = x.auctionid
+     LEFT JOIN accounts a ON m.buyerid = a.accountid 
+   WHERE m.answer !=0 AND x.accountid = :accountid";
     $stmtSell = $pdo->prepare($querySell);
-    $stmtSell->bindValue(':account_id', $account_id);
+    $stmtSell->bindValue(':accountid', $account_id);
     $stmtSell->execute();
 
-    // Zapytanie dla wiadomości kupowanych (naglowek)
+    // Zapytanie dla kupowanych (naglowek)
     $queryBuy = "SELECT 
-    DISTINCT m.auctionid, m.buyerid, a.firstname,a.lastname, x.title
+    DISTINCT m.auctionid, m.buyerid, x.accountid as seller, a.login, x.title
     FROM message m 
-    LEFT JOIN accounts a ON m.buyerid = a.accountid 
-    LEFT JOIN auctions x ON m.auctionid = x.auctionid
-    WHERE x.accountid = :accountid";
+     LEFT JOIN auctions x ON m.auctionid = x.auctionid
+     LEFT JOIN accounts a ON m.buyerid = a.accountid 
+   WHERE m.answer !=0 AND x.accountid <> :accountid";
     $stmtBuy = $pdo->prepare($queryBuy);
     $stmtBuy->bindParam(':accountid', $account_id);
     $stmtBuy->execute();
-
-    // Zapytanie dla wiadomości kupowanych (tresc)
-    $queryBuyM = "SELECT id, answer, date, description FROM message WHERE auctionid = 1 AND buyerid= 1;";
-    $stmtBuyM = $pdo->prepare($queryBuyM);
-    $stmtBuyM->execute();
 
 
 } catch (PDOException $e) {
@@ -63,92 +56,76 @@ try {
 
             <!--WIADOMOŚCI OD UZYTKOWNIKÓW-->
             <div class="tab-pane fade show active" id="buy" role="tabpanel" aria-labelledby="messageBuy-tab">
-                <div style="width: 20%; float: left;">
-                                <table class="table">
-                                    <tbody>
+
 
                 <?php
                 // Wyświetlanie wiadomości od zainteresowanych (naglowki)
                 while ($rowBuy = $stmtBuy->fetch(PDO::FETCH_ASSOC)) {
-                   // $title = $rowBuy[$title];
                 echo <<< TABLEMESSLIST
-                <tr>
-                    <th scope="row p-3">
-                    <a class="text-dark" style="text-decoration: none;" href="scripts/mesview.php?aid=$rowBuy[auctionid]&bid=$rowBuy[buyerid]"> 
-                        <span>$rowBuy[title]</span><br>
-                        <span style="text-decoration: none; font-size: small; ">$rowBuy[firstname] $rowBuy[lastname]</span> 
-                    </a>
-                    <br>
-                    </th>
-                </tr>
+                        <div class="row box p-3">  
+                            <br>                          
+                                <div class="box-text" >
+                                
+                                     <div style="width: 50%; float: left; ">
+                                        <h3>$rowBuy[login]</h3>
+                                        <p>
+                                            <i style="font-size: 0.9em">$rowBuy[title]</i>
+                                        </p>
+                                     </div>
+                                     
+                                     <div style="overflow: hidden; text-align: right;">
+                                     <br>
+                                       <a href="messageview.php?aid=$rowBuy[auctionid]&bid=$rowBuy[buyerid]&a=0" class="btn btn-outline-secondary">
+                                       <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-caret-right-fill" viewBox="0 0 16 16">
+                                          <path d="m12.14 8.753-5.482 4.796c-.646.566-1.658.106-1.658-.753V3.204a1 1 0 0 1 1.659-.753l5.48 4.796a1 1 0 0 1 0 1.506z"/>
+                                        </svg>
+                                        </a>
+                                     </div>
+                                     
+                                   
+                                </div>
+                                
+                            
+                            </div>
              TABLEMESSLIST;
                 }
                 ?>
-                </tbody>
-             </table>
-             </td>
-                    <br />
-            </div>
-            <div id="messview" style="width: 80%; float: left; overflow: hidden; padding: 0em 0em 0em 2em">
-                <table class="table table-striped">
-                    <tbody>
-                <?php
-                // Wyświetlanie wiadomości od zainteresowanych (tresc)
-                while ($rowBuyM = $stmtBuyM->fetch(PDO::FETCH_ASSOC)) {
-                    //print_r($rowBuy);
-
-                echo '<tr>';
-                  echo ' <td>';
 
 
-                        if($rowBuyM["answer"]==1){
-                        echo "<div style='text-align: right; font-size: 0.6em;'>".$rowBuyM["date"]."</div>
-                                <div style='text-align: right;'>".$rowBuyM["description"]."</div>";
-                        }else{
-                        echo "<div style='font-size: 0.6em;'>".$rowBuyM["date"]."</div>
-                        <div>".$rowBuyM["description"]."</div>";
-                        }
-
-                   echo' <br>';
-                    echo '</td>';
-                echo'</tr>';
-                }
-                $description='';
-                echo <<<SENDMESS
-                    <tr>
-                        <td>
-                        <form method='POST' action='scripts/message.php'>
-                            <div class="form-row p-3 text-right">
-                                <textarea class="form-control" name='description' id='description' value='description' required>$description</textarea><br>
-                                <input type='hidden' name='account_id' value='$account_id'>
-                                <a class="btn btn-secondary" type='submit'>
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-send" viewBox="0 0 16 16">
-                                        <path d="M15.854.146a.5.5 0 0 1 .11.54l-5.819 14.547a.75.75 0 0 1-1.329.124l-3.178-4.995L.643 7.184a.75.75 0 0 1 .124-1.33L15.314.037a.5.5 0 0 1 .54.11ZM6.636 10.07l2.761 4.338L14.13 2.576 6.636 10.07Zm6.787-8.201L1.591 6.602l4.339 2.76 7.494-7.493Z"/>
-                                    </svg>
-                                </a>
-                            </div>
-                        </form>
-                        </td>
-                    </tr>
-                SENDMESS;
-
-                ?>
-
-
-                    </tbody>
-                </table>
-            </div>
             </div>
 
 
             <div class="tab-pane fade" id="sell" role="tabpanel" aria-labelledby="messageSell-tab">
                 <br />
                 <?php
-                // Wyświetlanie wiadomości sprzedawanych
-                echo "Wiadomości sprzedawanych:<br>";
                 while ($rowSell = $stmtSell->fetch(PDO::FETCH_ASSOC)) {
-                    echo "Tytuł aukcji: " . $rowSell['title'] . "<br>";
-                    echo "Opis wiadomości: " . $rowSell['description'] . "<br><br>";
+                    echo <<< TABLEMESSLIST
+                        <div class="row box p-3">  
+                            <br>                          
+                                <div class="box-text" >
+                                
+                                     <div style="width: 50%; float: left; ">
+                                        <h3>$rowSell[login]</h3>
+                                        <p>
+                                            <i style="font-size: 0.9em">$rowSell[title]</i>
+                                        </p>
+                                     </div>
+                                     
+                                     <div style="overflow: hidden; text-align: right;">
+                                     <br>
+                                       <a href="messageview.php?aid=$rowSell[auctionid]&bid=$rowSell[buyerid]&a=1" class="btn btn-outline-secondary">
+                                       <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-caret-right-fill" viewBox="0 0 16 16">
+                                          <path d="m12.14 8.753-5.482 4.796c-.646.566-1.658.106-1.658-.753V3.204a1 1 0 0 1 1.659-.753l5.48 4.796a1 1 0 0 1 0 1.506z"/>
+                                        </svg>
+                                        </a>
+                                     </div>
+                                     
+                                   
+                                </div>
+                                
+                            
+                            </div>
+             TABLEMESSLIST;
                 }
                 ?>
             </div>
---
+++ messageview.php
@@ -0,0 +1,85 @@
+<?php
+require "constant/header.php";
+require 'scripts/connect.php';
+
+if (!isset($_SESSION['logged']['account_id'])||$_SESSION['logged']['account_id']==null) {
+    header("Location: index.php");
+
+}
+$account_id = $_SESSION['logged']['account_id'];
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $user, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $pdo->exec("USE $dbname");
+
+
+    // Zapytanie dla wiadomości (tresc)
+    $query = "SELECT id, answer, date, description FROM message 
+                        WHERE auctionid = :auctionid AND buyerid= :buyerid;";
+    $stmt = $pdo->prepare($query);
+    $stmt->bindParam(':auctionid', $_GET['aid']);
+    $stmt->bindParam(':buyerid', $_GET['bid']);
+    $stmt->execute();
+
+} catch (PDOException $e) {
+    echo "Błąd połączenia lub aktualizacji bazy danych: " . $e->getMessage();
+}
+?>
+                <div id="messview" style="width: 80%; float: left; overflow: hidden; padding: 0em 0em 0em 2em">
+
+                <table class="table table-striped">
+                    <tbody>
+
+                <?php
+                // Wyświetlanie wiadomości od zainteresowanych (tresc)
+                $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+                foreach ($rows as $row) {
+                    //print_r($rowBuy);
+
+                    echo '<tr>';
+                    echo ' <td>';
+
+
+                    if($row["answer"]==1){
+                        echo "<div style='text-align: right; font-size: 0.6em;'>".$row["date"]."</div>
+                                <div style='text-align: right;'>".$row["description"]."</div>";
+                    }else{
+                        echo "<div style='font-size: 0.6em;'>".$row["date"]."</div>
+                        <div>".$row["description"]."</div>";
+                    }
+
+                    echo' <br>';
+                    echo '</td>';
+                    echo'</tr>';
+                }
+                $description='';
+                if(!isset($_GET['a']) || $_GET['a'] == null){$_GET['a']=2;}
+                echo <<<SENDMESS
+                    <tr>
+                        <td>
+                        <form method='POST' action='scripts/mesview.php'>
+                            <div class="form-row p-3" >
+                                <div style="width: 90%; float: left;">
+                                    <textarea class="form-control" name='description' id='description' value='$description' required>$description</textarea><br>
+                                    <input type='hidden' name='auctionid' value='$_GET[aid]'>
+                                    <input type='hidden' name='buyerid' value='$_GET[bid]'>
+                                    <input type='hidden' name='answer' value='$_GET[a]'>
+                                </div>
+                               <div style=" overflow: hidden; float: right;">
+                                    <button class="btn btn-secondary">
+                                    <a type='submit'>
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-send" viewBox="0 0 16 16">
+                                            <path d="M15.854.146a.5.5 0 0 1 .11.54l-5.819 14.547a.75.75 0 0 1-1.329.124l-3.178-4.995L.643 7.184a.75.75 0 0 1 .124-1.33L15.314.037a.5.5 0 0 1 .54.11ZM6.636 10.07l2.761 4.338L14.13 2.576 6.636 10.07Zm6.787-8.201L1.591 6.602l4.339 2.76 7.494-7.493Z"/>
+                                        </svg>
+                                    </a>
+                                    </button>
+                               </div>
+                            </div>
+                        </form>
+                        </td>
+                    </tr>
+                SENDMESS;
+
+                ?>
+
